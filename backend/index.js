efc70ff33a0bd36370aa94e2fdb7b5ada5668dbf
yannlelorier <yannllb97@gmail.com>
2021-03-10 16:12:24 +0000

    beginning endpoints implementations (dummy vars)

--- backend/index.js
+++ backend/index.js
@@ -14,9 +14,11 @@ var port = process.env.PORT || 3000;
 
 //Tell express to use the body parser module
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //Tell express to use cors -- enables CORS for this backend
-app.use(cors());  
+app.use(cors());
+
 
 //The following is an example of an array of three boards. 
 var boards = [
@@ -32,6 +34,10 @@ var tasks = [
     { id: '3', boardId: '3', taskName: "Prepare assignment 2", dateCreated: new Date(Date.UTC(2021, 00, 10, 16, 00)), archived: true }
 ];
 
+//TODO correct this
+var boardCount = boards.length+1;
+var taskCount = tasks.length;
+
 //Your endpoints go here
 
 //Inital page redirects to boards by default
@@ -41,13 +47,19 @@ app.get('/', function(req, res) {
 
 //get all boards
 app.get('/boards/', function(req, res) {
-    res.status(200);
     try {
-        res.status(200).json({
-            //TODO only send ID, name and description
-            data: boards
+        let resArray = [];
 
-        });
+        for (let i=0; i<boards.length; i++){
+            resArray.push({
+                'id':boards[i].id,
+                'name':boards[i].name,
+                'description':boards[i].description
+            });
+        }
+        
+        return res.status(200).send(resArray);
+
     }catch (err){
         res.status(400).json({
             message: "Oops, something went wrong.",
@@ -62,10 +74,14 @@ app.get('/boards/:boardId/', function(req, res) {
     let boardId = req.params.boardId;
 
     try {
-        let board = boards.find(board => board.id === boardId);
-        res.status(200).json({
-            data: board
-        });
+        let i;
+        for (i=0; i<boards.length; i++) {
+            if(boards[i].id === boardId) {
+                res.status(200).json(boards[i]);
+                return;
+            }
+        }
+        res.status(404).send('oopsies no board found.');
     } catch (err) {
         res.status(400).json({
             message: "Some error occured",
@@ -75,13 +91,46 @@ app.get('/boards/:boardId/', function(req, res) {
 })
 
 //create a board
-app.post('/boards/', function(req, res) {
-    res.status(200);
+app.post('/boards/', async(req, res) => {
+    const name = await req.body.name;
+    const description = await req.body.description;
+
+    if (req.body.name !== '') {
+        let newBoard = {
+            id: boardCount.toString(),
+            name: name,
+            description: description,
+            tasks: []
+        }
+        boardCount++;
+        boards.push(newBoard);
+        console.log("Created a new board.");
+        res.sendStatus(200);
+
+    }else {
+        return res.status(405).send('Name cannot be empty');
+    }
+    
 })
 
 //update a board
 app.post('/boards/:boardId/', function(req, res) {
-    res.status(200);
+    let boardId = req.params.boardId;
+    
+    if (req.body.name !== '') {
+        let newBoard = {
+            id: boardCount,
+            name: req.body.name,
+            description: req.body.description,
+            tasks: []
+        }
+        boardCount++;
+        boards.push(boards);
+        return res.status(200).send(newBoard);
+
+    }else {
+        return res.status(405).send('Name cannot be empty');
+    }
 })
 
 //delete a specific board
@@ -90,19 +139,65 @@ app.delete('/boards/:boardId/', function(req, res) {
 })
 
 //getting the tasks assigned to a specific board
-app.get('boards/:boardId/tasks/', function(req, res) {
+app.get('/boards/:boardId/tasks/', function(req, res) {
+    let boardId = req.params.boardId;
+    let resArray = [];
+
+    let i;
+    let j;
+
+    if (!req.query.sort) {
+        for (i=0; i<boards.length; i++) {
+            if (boards[i].id === boardId) {
+                for (j=0; j<boards[i].tasks.length; j++) {
+                    resArray.push(tasks[boards[i].tasks[j]]);
+                }
+            }
+        }
+    }
+    //TODO sort argument
+    // else if (req.query.sort === 'id') {
+    //     for (i=0; i<boards.length; i++) {
+    //     }
+    // }else if (req.query.sort === 'taskName') {
+
+    // } else if (req.query.sort === 'dateCreated') {
+
+    // }else {
+    //     return res.status(404).send('Unrecognized sorting argument. Check your query');
+    // }
+
+    return res.status(200).send(resArray)
+})
+
+app.get('/boards/:boardId/tasks/:taskId/', function(req, res) {
+    let boardId = req.params.boardId;
+    let taskId = req.params.taskId;
+    let i;
+    let j;
+
+    for (i=0; i<boards.length; i++) {
+        if (boards[i].id === boardId) {
+            for (j=0; j<boards[i].tasks.length; j++) {
+                let thisTask = boards[i].tasks[j];   
+
+                if (parseInt(taskId) === parseInt(thisTask)) {
+                    return res.status(200).send(tasks[thisTask]);
+                }
+            }
+        }else {
+            return res.status(404).send('No boardId matches the one in the query.');
+        }
+    } 
+    return res.status(404).send('No task found with that ID for this board.');
+    
+})
+
+app.post('/boards/:boardId/tasks/', function(req, res) {
     res.status(200);
 })
 
-app.get('boards/:boardId/tasks/:taskId', function(req, res) {
-    res.status(200);
-})
-
-app.post('boards/:boardId/tasks/', function(req, res) {
-    res.status(200);
-})
-
-app.delete('boards/:boardId/tasks/:taskId', function(req, res) {
+app.delete('/boards/:boardId/tasks/:taskId', function(req, res) {
     res.status(200);
 })
 
